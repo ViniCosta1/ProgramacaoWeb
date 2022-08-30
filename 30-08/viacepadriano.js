13c11a5025b54d2f50f37799217c632e5b835490
ViniCosta1 <vinicosta154@hotmail.com>
2022-08-30 08:27:45 -0300

    ViaCEP do Adriano

---
+++ 30-08/viacepadriano.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    $(".btn").click(function (e) {
+        e.preventDefault();
+        var cep = document.getElementById("CEP").value;
+        console.log(cep);
+        $.ajax({
+            type: "GET",
+            url: `https://viacep.com.br/ws/${cep}/json/`,
+            success: function (data) {
+                $("#logradouro").val(data.logradouro);
+                $("#bairro").val(data.bairro);
+                $("#complemento").val(data.complemento);
+                $("#localidade").val(data.localidade);
+                $("#UF").val(data.uf);
+
+                if (data.complemento == "") {
+                    $("#complemento").val("SEM COMPLEMENTO");
+                }
+            }
+        });
+    });
+});
